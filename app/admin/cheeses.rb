034f31f66d1f82e23dd54af72e61db626f5da36f
Raghu Betina <raghu@firstdraft.com>
2018-06-11 20:22:08 +0000

    Whitelist columns in Cheese for ActiveAdmin

--- app/admin/cheeses.rb
+++ app/admin/cheeses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Cheese do
+
+ permit_params :name, :milk, :age, :variety, :regions_id, :producers_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
