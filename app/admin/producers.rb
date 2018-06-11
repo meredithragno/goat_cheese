55a261af8edb86d3b89e3ecef6e8855f399d1af9
Raghu Betina <raghu@firstdraft.com>
2018-06-11 20:22:19 +0000

    Whitelist columns in Producer for ActiveAdmin

--- app/admin/producers.rb
+++ app/admin/producers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Producer do
+
+ permit_params :regions_id, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
